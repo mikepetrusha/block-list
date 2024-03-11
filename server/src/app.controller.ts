4a12e0955017b7a866d481656bad1c8d96f08ad6
Mike Petrusha <petrushamike@gmail.com>
2024-03-11 17:06:07 +0300

    init: initial commit

---
+++ server/src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+import { PrismaClient } from '@prisma/client';
+import { log } from 'console';
+const prisma = new PrismaClient();
+
+class HelloWorldDto {
+  @ApiProperty()
+  message: string;
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @ApiOkResponse({ type: HelloWorldDto })
+  async getHello(): Promise<HelloWorldDto> {
+    const users = await prisma.user.findMany();
+    console.log(users);
+    return { message: this.appService.getHello() };
+  }
+}
