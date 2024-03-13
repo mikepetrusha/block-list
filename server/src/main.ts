3592e2ac7fc7f811eaccab27859d97ecbe0401a5
Mike Petrusha <petrushamike@gmail.com>
2024-03-13 11:28:00 +0300

    feat: add backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [
+      'http://localhost:3001',
+      'chrome-extension://jcmfnmkpolncpgmjlhlknajklonbkcdc',
+    ],
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
